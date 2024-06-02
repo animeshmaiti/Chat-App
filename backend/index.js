c9098b1a1eb0d1502867790c21f46f119c0d1163
Animesh Maiti <a.maiti770@gmail.com>
2024-06-02 21:24:55 +0530

    init express server user model connect mongo

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import  {configDotenv}  from "dotenv";
+import authRoutes from "./Routes/auth.js";
+import mongoConnect from "./db/mongoConnect.js";
+
+configDotenv();
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(port, () => {
+    mongoConnect();
+    console.log(`Server is running on port http://localhost:${port}/`);
+});
